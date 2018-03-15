7ad21d3984ea290783db0c1d9ea71228fbd95db7
Zhylf <zhyl529@163.com>
2018-03-15 11:49:17 +0800

    瀑布流核心算法

--- Lottery/Lottery/ZFWaterflowLayout.m
+++ Lottery/Lottery/ZFWaterflowLayout.m
@@ -7,17 +7,43 @@
 //
 
 #import "ZFWaterflowLayout.h"
+//默认的行数
+static const CGFloat ZFDefaultColumnCount = 3;
+//每一列之间的间距
+static const CGFloat ZFDefaultColumnMargin = 10;
+//每一行之间的间距
+static const CGFloat ZFDefaultRowMargin = 10;
+//边缘间距
+static const UIEdgeInsets ZFDefaultEdgeInsets = {10,10,10,10};
 @interface ZFWaterflowLayout ()
+
+/**
+ 存放所有cell的布局属性
+ */
 @property (nonatomic,strong) NSMutableArray *attriArray;
+
+/**
+ 存放左右列的最大高度
+ */
+@property (nonatomic,strong) NSMutableArray *columnHeights;
 
 @end
 
 @implementation ZFWaterflowLayout
+- (NSMutableArray *)columnHeights {
+    if (!_columnHeights) {
+        _columnHeights = [NSMutableArray array];
+    }
+    return _columnHeights;
+    
+}
+
 - (NSMutableArray *)attriArray {
     if (!_attriArray) {
         _attriArray = [NSMutableArray array];
     }
     return _attriArray;
+    
 }
 
 /**
@@ -26,7 +52,13 @@
 - (void)prepareLayout
 {
     [super prepareLayout];
-    //清楚之前的
+    //清楚之前的计算的所有高度
+    [self.columnHeights removeAllObjects];
+    for (NSInteger i = 0; i < ZFDefaultColumnCount; i++) {
+        [self.columnHeights addObject:@(ZFDefaultEdgeInsets.top)];
+    }
+    
+    //清楚之前的布局属性
     [self.attriArray removeAllObjects];
     //开始创建每一个cell对应的布局属性
     NSInteger count = [self.collectionView numberOfItemsInSection:0];
@@ -58,14 +90,60 @@
 {
     //创建布局属性
     UICollectionViewLayoutAttributes *attri = [UICollectionViewLayoutAttributes layoutAttributesForCellWithIndexPath:indexPath];
+    CGFloat collectionViewW = self.collectionView.frame.size.width;
+    
     //设置布局属性的frame
-    attri.frame = CGRectMake(arc4random_uniform(300), arc4random_uniform(300), arc4random_uniform(300), arc4random_uniform(300));
+    CGFloat w = (collectionViewW - ZFDefaultEdgeInsets.left - ZFDefaultEdgeInsets.right - (ZFDefaultColumnCount - 1) * ZFDefaultColumnMargin) / ZFDefaultColumnCount;
+    CGFloat h = 50 + arc4random_uniform(150);
+    
+    //找出高度最小的那一行
+    /*
+    __block NSInteger destColumn = 0;
+    __block CGFloat minColumnHeight = MAXFLOAT;
+    [self.columnHeights enumerateObjectsUsingBlock:^(NSNumber *columnHeightNumber, NSUInteger idx, BOOL * _Nonnull stop) {
+        CGFloat columnHeight = columnHeightNumber.doubleValue;
+        if (minColumnHeight > columnHeight) {
+            minColumnHeight = columnHeight;
+            destColumn = idx;
+        }
+    }];
+     */
+    NSInteger destColumn = 0;
+    CGFloat minColumnHeight = [self.columnHeights[0] doubleValue];
+    for (NSInteger i = 0; i < ZFDefaultColumnCount; i++) {
+        //取出第i列高度
+        CGFloat columnHeight = [self.columnHeights[i] doubleValue];
+        if (minColumnHeight > columnHeight) {
+            minColumnHeight = columnHeight;
+            destColumn = i;
+        }
+    }
+    
+    CGFloat x = ZFDefaultEdgeInsets.left + destColumn * (w + ZFDefaultColumnMargin);
+    
+    CGFloat y= minColumnHeight;
+    if (y != ZFDefaultEdgeInsets.top) {
+        y += ZFDefaultRowMargin;
+    }
+    attri.frame = CGRectMake(x, y, w, h);
+    //更新最短那列的高度
+    self.columnHeights[destColumn] = @(CGRectGetMaxY(attri.frame));
+    
     return attri;
 }
 
 - (CGSize)collectionViewContentSize
 {
-    return CGSizeMake(0, 1000);
+    CGFloat maxColumnHeight = [self.columnHeights[0] doubleValue];
+    for (NSInteger i = 0; i < ZFDefaultColumnCount; i++) {
+        //取出第i列高度
+        CGFloat columnHeight = [self.columnHeights[i] doubleValue];
+        if (maxColumnHeight < columnHeight) {
+            maxColumnHeight = columnHeight;
+            
+        }
+    }
+    return CGSizeMake(0, maxColumnHeight + ZFDefaultEdgeInsets.bottom);
 }
 
 
